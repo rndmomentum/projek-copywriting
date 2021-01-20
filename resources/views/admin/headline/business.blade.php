84040feac0f83a1b0987f3fe8a12d321c3593192
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-01-20 11:07:26 +0800

    Update business.blade.php

--- resources/views/admin/headline/business.blade.php
+++ resources/views/admin/headline/business.blade.php
@@ -9,6 +9,12 @@
 @endsection
 
 @section('content')
+@if(session('success'))
+    <div class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        {{ session('success') }}
+    </div>
+@endif
 <div class="panel panel-headline">
     <div class="panel-heading">
         <h3 class="panel-title">Add Business</h3>
