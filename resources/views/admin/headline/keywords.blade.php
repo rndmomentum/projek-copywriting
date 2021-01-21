a5653cde3417f2316c2bc449974e82b65df5282f
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-01-21 10:44:04 +0800

    Add page

---
+++ resources/views/admin/headline/keywords.blade.php
@@ -0,0 +1,73 @@
+@extends('admin.layouts.app')
+
+@section('title')
+    Add Keywords
+@endsection
+
+@section('css')
+    
+@endsection
+
+@section('content')
+@if(session('success'))
+    <div class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        {{ session('success') }}
+    </div>
+@endif
+<div class="panel panel-headline">
+    <div class="panel-heading">
+        <h3 class="panel-title">Add Keywords</h3>
+        <p class="panel-subtitle">Below list will be use on headline</p>
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <form action="{{ url('admin/headline/emotion/store') }}" method="POST">
+                @csrf
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="emotion" placeholder="Your text here" required>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="panel panel-headline">
+    <div class="panel-heading">
+        <h3 class="panel-title">List Keywords</h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-hover">
+                    <thead>
+                        <th>#</th>
+                        <th>Keywords</th>
+                        <th>Created At</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        @foreach($emotion as $e)
+                        <tr>
+                            <td>{{ $e->emotion_id }}</td>
+                            <td>{{ $e->emotion_text }}</td>
+                            <td>{{ $e->created_at }}</td>
+                            <td><button type="button" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('js')
+    
+@endsection
