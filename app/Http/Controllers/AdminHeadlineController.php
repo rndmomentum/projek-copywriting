eb4ef90d413ee4f3fce90718896d65b732963144
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-01-22 11:41:21 +0800

    Update AdminHeadlineController.php

--- app/Http/Controllers/AdminHeadlineController.php
+++ app/Http/Controllers/AdminHeadlineController.php
@@ -20,7 +20,9 @@ class AdminHeadlineController extends Controller
         $keywords = Keywords::all('keyword_text');
 
         $test = array($keywords);
-        echo array_values($test);
+
+        echo $keywords->implode('keyword_text', ', ');
+        //echo array_values($test);
 
         //return view('admin.headline.create', compact('keywords'));
     }
