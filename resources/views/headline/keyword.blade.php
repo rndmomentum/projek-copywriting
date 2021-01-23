27e3e2c248385c4e05abd560a4c7fe9ad849af0a
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-01-23 11:44:53 +0800

    Update keyword.blade.php

--- resources/views/headline/keyword.blade.php
+++ resources/views/headline/keyword.blade.php
@@ -22,7 +22,7 @@
                 <a href="{{ url('headline') }}/{{ $business }}/{{ $emotion }}/{{ $keyword->keyword }}" class="text-decoration-none">
                     <div class="card shadow">
                         <div class="card-body bg-primary text-light py-4 px-4">
-                            <h6 class="card-title"><b>{{ $keyword->keyword }}</b></h6>
+                            <h5 class="card-title text-center"><b>{{ $keyword->keyword }}</b></h5>
                         </div>
                     </div>
                 </a>
